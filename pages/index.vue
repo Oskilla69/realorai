5d71e23bffaa26b50394827793a6412e060449a7
oskilla68 <oscar.fan1@gmail.com>
2023-04-26 06:47:36 +1000

    [test] on vercel

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,23 @@
   <PageWrapper>
     <Aidle banner-size="full">
       <template #title-slot>
+        <h1>heyhey: {{ envVal }}</h1>
+        <h1>URL: {{ url }}</h1>
+        <h1>Public URL: {{ publicUrl }}</h1>
         <div class="w-full flex justify-center mt-20">
           <div class="flex flex-row gap-4">
-            <button>Dailies</button>
-            <button>Endless</button>
+            <button
+              class="rounded-md secondary-background tertiary-color p-4 text-2xl"
+              @click="() => $router.push('/daily')"
+            >
+              Dailies
+            </button>
+            <button
+              class="rounded-md secondary-color tertiary-background p-4 text-2xl"
+              @click="() => $router.push('/endless')"
+            >
+              Endless
+            </button>
           </div>
         </div>
       </template>
@@ -13,6 +26,10 @@
   </PageWrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const envVal = process.env;
+const url = useRuntimeConfig().supabasePublicKey;
+const publicUrl = useRuntimeConfig().public.supabasePublicKey;
+</script>
 
 <style scoped></style>
