773c43a235520c8113e3b505ab86cc7f36313ab6
oskilla68 <oscar.fan1@gmail.com>
2023-04-26 07:22:11 +1000

    [remove] test code. doesn't work lmao

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,6 @@
   <PageWrapper>
     <Aidle banner-size="full">
       <template #title-slot>
-        <h1>URL: {{ url }}</h1>
-        <h1>Public URL: {{ publicUrl }}</h1>
-        <h1>Config {{ this.$config }}</h1>
         <div class="w-full flex justify-center mt-20">
           <div class="flex flex-row gap-4">
             <button
@@ -26,9 +23,6 @@
   </PageWrapper>
 </template>
 
-<script setup lang="ts">
-const url = useRuntimeConfig().supabasePublicKey;
-const publicUrl = useRuntimeConfig().public.supabasePublicKey;
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
